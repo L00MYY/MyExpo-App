0bbe79457bbde440da9a171e6bb204fe2320fbfe
L00MYY <160758553+L00MYY@users.noreply.github.com>
2024-08-06 00:14:09 -0600

    Se corrigio direccion de IP

--- src/utils/Constantes.js
+++ src/utils/Constantes.js
@@ -1,4 +1,4 @@
 //IP de donde se encuentra la Api, en este caso la dirección
 //Ip de la maquina local
 //utilizar la direccion IP del servidor y no localhost
-export const IP = 'http://10.10.1.16//MYEXPO-APP/api';
+export const IP = 'http://192.168.1.31';
