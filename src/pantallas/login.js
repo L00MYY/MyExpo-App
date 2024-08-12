d6914f4d72c552d5a83071f6d7a258c6193af112
L00MYY <160758553+L00MYY@users.noreply.github.com>
2024-08-11 23:55:15 -0600

    Se corrigio el nombre de una variable

--- src/pantallas/login.js
+++ src/pantallas/login.js
@@ -8,15 +8,15 @@ import { useFocusEffect } from '@react-navigation/native';
 // Componente principal
 export default function Login({ navigation }) {
     const ip = Constantes.IP;
- 
+
     // Estados
     const [isContra, setIsContra] = useState(true);  // Estado para mostrar/ocultar la contraseña
-    const [usuario, setUsuario] = useState(''); // Estado para el campo del usuario
+    const [correo, setCorreo] = useState(''); // Estado para el campo del usuario
     const [contrasenia, setContrasenia] = useState(''); // Estado para el campo de la contraseña
     const [showAlert, setShowAlert] = useState(false); // Estado para mostrar/ocultar la alerta
     const [alertMessage, setAlertMessage] = useState(''); // Estado para el mensaje de la alerta
     const [showProgress, setShowProgress] = useState(false); // Estado para mostrar/ocultar el indicador de progreso
- 
+
     // Efecto para cargar los detalles del carrito al cargar la pantalla o al enfocarse en ella
     useFocusEffect(
         // La función useFocusEffect ejecuta un efecto cada vez que la pantalla se enfoca.
@@ -24,16 +24,16 @@ export default function Login({ navigation }) {
             validarSesion(); // Llama a la función getDetalleCarrito.
         }, [])
     );
- 
+
     // Función para validar si hay una sesión activa
     const validarSesion = async () => {
         try {
             const response = await fetch(`${ip}/services/public/profesorService.php?action=getUser`, {
                 method: 'GET'
             });
- 
+
             const data = await response.json();
- 
+
             if (data.status === 1) {
                 navigation.navigate('TabNavigator'); // Navega a la siguiente pantalla
             } else {
@@ -44,16 +44,16 @@ export default function Login({ navigation }) {
             showAlertWithMessage('Ocurrió un error al validar la sesión'); // Muestra un mensaje de error
         }
     };
- 
+
     // Función para cerrar la sesión activa
     const cerrarSesion = async () => {
         try {
-            const response = await fetch(`${ip}/services/public/profesorService.php?action=logOut`, {
+            const response = await fetch(`${ip}/services/serviceProfesores/profesor.php?action=logOut`, {
                 method: 'GET'
             });
- 
+
             const data = await response.json();
- 
+
             if (data.status) {
                 console.log("Sesión Finalizada");
             } else {
@@ -64,35 +64,35 @@ export default function Login({ navigation }) {
             showAlertWithMessage('Ocurrió un error al cerrar sesión'); // Muestra un mensaje de error
         }
     };
- 
+
     // Función para mostrar una alerta con un mensaje específico
     const showAlertWithMessage = (message, showProgressIndicator = false) => {
         setAlertMessage(message);
         setShowProgress(showProgressIndicator);
         setShowAlert(true);
     };
- 
+
     // Función para manejar el proceso de inicio de sesión
     const handlerLogin = async () => {
         if (!usuario.trim() || !contrasenia.trim()) {
             showAlertWithMessage('Por favor completa todos los campos'); // Verifica que los campos no estén vacíos
             return;
         }
- 
+
         showAlertWithMessage('Iniciando sesión...', true); // Muestra un mensaje de progreso
- 
+
         try {
             const formData = new FormData();
             formData.append('correo', usuario);
             formData.append('clave', contrasenia);
- 
+
             const response = await fetch(`${ip}/services/public/profesorService.php?action=logIn`, {
                 method: 'POST',
                 body: formData
             });
- 
+
             const data = await response.json();
- 
+
             if (data.status) {
                 setContrasenia('');
                 setUsuario('');
@@ -112,23 +112,23 @@ export default function Login({ navigation }) {
             setShowProgress(false); // Oculta el indicador de progreso
         }
     };
- 
+
     // Función para navegar a la pantalla de registro
     const irRegistrar = async () => {
         navigation.navigate('Registro');
     };
- 
+
     // Efecto para validar la sesión al montar el componente
     useEffect(() => {
         validarSesion();
     }, []);
- 
+
     return (
         <View style={styles.container}>
             <Text style={styles.headerText}>Iniciar Sesión</Text>
             <TextInput
-                value={usuario}
-                onChangeText={setUsuario}
+                value={correo}
+                onChangeText={setCorreo}
                 style={styles.input}
                 placeholder="Correo electrónico"
                 placeholderTextColor="#aaa"
@@ -174,7 +174,7 @@ export default function Login({ navigation }) {
         </View>
     );
 }
- 
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
