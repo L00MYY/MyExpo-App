7c4296fb7e0cb7d1b93a3ad5001456d4ea4dfb05
Diegozj <136825055+Diegozj@users.noreply.github.com>
2024-08-12 10:31:25 -0600

    Avance de no se de juarez

--- src/pantallas/SplashScreen.js
+++ src/pantallas/SplashScreen.js
@@ -9,7 +9,7 @@ export default function SplashScreen2() {
   return (
       <View style={styles.container}>
         <Image
-          source={require('../img/logo_rical.png')}
+          source={require('../img/logo-ricaldone.png')}
           style={styles.image}
           resizeMode="contain"
         />
@@ -31,6 +31,6 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '100%',
     height: '100%',
-    backgroundColor: '#FFFFFF',
+    backgroundColor: '#000',
   },
 });
